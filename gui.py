bf3c01b39f42c82b47331221042adebfa12d6fe0
AnnTarek <anntarek125@gmail.com>
2024-04-26 01:22:11 +0300

    grid initialization GUI and insertion

--- gui.py
+++ gui.py
@@ -3,19 +3,54 @@ from tkinter import Canvas
 from tkinter import Image
 from PIL import ImageTk
 import tkinter as tk
+
 # Constants
 WIDTH = 950
 HEIGHT = 680
 MARGIN = 80
 SIDE_MARGIN = 30
-CELL_SIZE = ((WIDTH - 2 * MARGIN) / 9) -SIDE_MARGIN
+CELL_SIZE = ((WIDTH - 2 * MARGIN) / 9) - SIDE_MARGIN
 BG_IMAGE = "Sudoku_bg.png"
+BUTTONS = []
+INPUT_BUTTONS = []
+ROW, COLUMN = 0, 0
+
+rows, cols = 9, 9
+BUTTONS = [[0] * cols for _ in range(rows)]
+
+INPUT_BUTTONS = [0] * 9
+
+def show_hint_system():
+    pass
 
 def erase_canvas():
     canvas.delete("all")
-    
+
+def edit_cell(text):
+    '''Edit a cell in the board'''
+    if ROW != -1 and COLUMN != -1:
+         BUTTONS[ROW][COLUMN].config(text=text)
+    for i in range(9):
+        INPUT_BUTTONS[i].config(bg="#d9d9d9")
+    BUTTONS[ROW][COLUMN].config(highlightthickness=0)
+
+
+def initialize_board(initial_state):
+    '''Initialize board with numbers of the initial state of the game'''
+    global ROW, COLUMN
+    for i in range(len(initial_state)):
+        row = i // 9
+        col = i % 9
+        ROW = col
+        COLUMN = row
+        if initial_state[i] != "0":
+            edit_cell(initial_state[i])
+            BUTTONS[col][row].config(state="disabled")
+            BUTTONS[col][row].config(bg="#4d4d4d", font = ("Helvetica", 12, "bold"))
+
 def create_grid():
     # Create a 9x9 grid
+    global ROW, COLUMN
     for i in range(10):
         color = "black" if i % 3 == 0 else "gray"
         #vertical lines
@@ -33,33 +68,68 @@ def create_grid():
         canvas.create_line(x0, y0, x1, y1, fill=color)
         print("Ana hena")
     create_grid_buttons()
+    create_input_buttons()
+    initialize_board("".join(["700009050",
+                 "040050700",
+                 "003000010",
+                 "208160000",
+                 "000308000",
+                 "000027108",
+                 "080000500",
+                 "009010030",
+                 "060200004"]))
+    ROW, COLUMN = -1, -1
+
+
+def create_window():
+    create_grid()
+    canvas.create_window(WIDTH- WIDTH//6, HEIGHT//3, window=tk.Button(window, text="HINT", command=show_hint_system, bg="#c4bebe", fg="black", width=10))
+
+def change_highlights():
+    BUTTONS[ROW][COLUMN].config(highlightthickness = 0)
+
+def highlight_input_buttons(row, column):
+    global ROW 
+    global COLUMN 
+    if ROW != row or COLUMN!= column:
+        change_highlights()
+    ROW = row
+    COLUMN = column
+    for i in range(9):
+        INPUT_BUTTONS[i].config(bg="#dc8a89")
+    BUTTONS[ROW][COLUMN].config(highlightthickness=2, highlightbackground="black")
+
+def create_input_buttons():
+    for i in range(9):
+        x = MARGIN + i * CELL_SIZE + SIDE_MARGIN
+        y = HEIGHT - MARGIN + 50
+        INPUT_BUTTONS[i] = tk.Button(window, text=i + 1, bg="#d9d9d9",highlightthickness=1, highlightbackground="black", fg="black", width=2, height=2, command=lambda i=i: edit_cell(i + 1))
+        canvas.create_window(x, y, window=INPUT_BUTTONS[i])
 
 def create_grid_buttons():
     for i in range(9):
         for j in range(9):
             x = MARGIN + i * CELL_SIZE + SIDE_MARGIN 
             y = MARGIN + j * CELL_SIZE + (1/2 * CELL_SIZE)
-            canvas.create_window(x, y, window=tk.Button(window, text="1", bg="#c4bebe", fg="black", width=2, height=2))
-            
+            BUTTONS[i][j] = tk.Button(window, text=" ", bg="#d9d9d9", fg="black", width=2, height=2, command=lambda i=i, j=j: highlight_input_buttons(i, j))
+            canvas.create_window(x, y, window=BUTTONS[i][j])
 
      
 def mode_1():
     erase_canvas()
-    create_grid()
+    create_window()
     
-    
-
-
 def mode_2():
     erase_canvas()
-    create_grid()
+    create_window()
 
 def mode_3():
     erase_canvas()
-    create_grid()
+    create_window()
 
 
 # Create the main window
+
 window = Tk()
 window.title("Suduko Solver")
 window.resizable(False, False)
